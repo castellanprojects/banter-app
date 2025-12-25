d74c2571c8cf49dd2f7b5bc7f035f4ef210ff4ca
Midgard <nonconformistical@gmail.com>
2025-12-25 14:33:57 +0100

    Profile Updating Endpoint Implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { ENV } from "./lib/env.js";
 import path from "path";
 
@@ -14,7 +15,8 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json()) // req.body
+app.use(express.json()); // req.body
+app.use(cookieParser());
 
 // Routes
 
