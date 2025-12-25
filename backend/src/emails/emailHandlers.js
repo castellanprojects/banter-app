4c002a773188bdd2052e36b12c4b66ff48a6602d
Midgard <nonconformistical@gmail.com>
2025-12-25 12:48:30 +0100

    Welcome email sending implementation

---
+++ backend/src/emails/emailHandlers.js
@@ -0,0 +1,20 @@
+import { resendClient, sender } from "../lib/resend.js";
+import { createWelcomeEmailTemplate } from "../emails/emailTemplates.js";
+
+export const sendWelcomeEmail = async (email, name, clientURL) => {
+
+    const {data, error} = await resendClient.emails.send({
+        from: `${sender.name} <${sender.email}>`,
+        to: email,
+        subject: "Welcome to Banter!",
+        html: createWelcomeEmailTemplate(name, clientURL)
+    })
+
+    if (error) {
+        console.error ("Error sending welcome email: ", error);
+        throw new Error("Failed to send a welcome email.");
+    }
+
+    console.log("Welcome email was sent successfully! ", data);
+
+}
---
+++ backend/src/lib/env.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+
+export const ENV = {
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    NODE_ENV: process.env.NODE_ENV,
+    CLIENT_URL: process.env.CLIENT_URL,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME
+}
+
+// PORT = 3000
+// MONGO_URI = mongodb+srv://nonconformistical_db_user:ZrU4otiOpjepXBEd@bantercluster.y7oyuab.mongodb.net/banter_db?appName=BanterCluster
+// NODE_ENV = development
+// JWT_SECRET = 1jd9cn39ahjf83jq84h6b3j7hd93u6
+// RESEND_API_KEY = "re_XVbHqp7w_BN1TaTLGRSCiekLAGzNxpLVR"
+// EMAIL_FROM = "onboarding@resend.dev"
+// EMAIL_FROM_NAME = "Midgard"
+// CLIENT_URL = http://localhost:5137
