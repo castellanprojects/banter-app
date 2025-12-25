6710670865eb44537363790984a7267bc402c0ab
Midgard <nonconformistical@gmail.com>
2025-12-25 10:51:08 +0100

    package.json update

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log("MongoDB connection ensured successfully: ", conn.connection.host)
+        const { MONGO_URI } = process.env;
+        if (!MONGO_URI) throw new Error("MONGO_URI is not set!");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connection ensured successfully: ", conn.connection.host);
     }
 
     catch (error) {
