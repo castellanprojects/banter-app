476fff797ee26802e45615c0e1c25b5aa8d0bc92
Midgard <nonconformistical@gmail.com>
2025-12-26 16:13:50 +0100

    #	modified:   frontend/package-lock.json #	modified:   frontend/package.json #	new file:   frontend/postcss.config.js #	deleted:    frontend/src/App.css #	modified:   frontend/src/App.jsx #	deleted:    frontend/src/assets/react.svg #	modified:   frontend/src/index.css #	new file:   frontend/src/lib/axios.js #	modified:   frontend/src/main.jsx #	new file:   frontend/src/pages/ChatPage.jsx #	new file:   frontend/src/pages/LoginPage.jsx #	new file:   frontend/src/pages/SignUpPage.jsx #	new file:   frontend/src/store/useAuthStore.js #	new file:   frontend/tailwind.config.js

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>Chat</div>
+  )
+}
+
+export default LoginPage
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default LoginPage
